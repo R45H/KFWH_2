041455482219c1d91ef6311973d26c989fd13f22
Олег <gmr45h@gmail.com>
2017-02-17 13:55:23 +0300

    Added the jScrollPane

--- app/src/libs.js
+++ app/src/libs.js
@@ -1,3 +1,5 @@
 @@include('../libs/jquery/dist/jquery.js');
 @@include('../libs/bootstrap/dist/js/bootstrap.js');
 @@include('../libs/owl.carousel/dist/owl.carousel.js');
+@@include('../libs/jScrollPane/script/jquery.mousewheel.js');
+@@include('../libs/jScrollPane/script/jquery.jscrollpane.js');
